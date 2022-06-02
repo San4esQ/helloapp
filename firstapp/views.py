06cf2c274ab2d58d786d272c604901103a509dff
Alexsandr Voronkov <voronkov_sasha2908@mail.ru>
2022-06-02 16:05:14 +0500

    Upgrade index.html and views.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    header = "Personal Data"
+    langs = ["English", "German", "Spanish"]
+    user = {"name" : "Tom", "age" : 23}
+    addr = ("Абрикосовая", 23, 45)
 
+    data = {"header":header, "langs":langs, "user":user, "address": addr}
+    return render(request, "index.html", context=data)
