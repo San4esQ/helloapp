ed3feda37f39c406db968879430c8e5dbded031b
Alexsandr Voronkov <voronkov_sasha2908@mail.ru>
2022-06-02 14:44:36 +0500

    Upgrade commit: add pattern

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,8 +1,5 @@
-from django.http import HttpResponse
-def products(request, productid):
-    output = "<h2>Product № {0}</h2>".format(productid)
-    return HttpResponse(output)
+from django.shortcuts import render
 
-def users(request, id, name):
-    output = "<h2>User</h2><h3>id: {0} name: {1}</h3>".format(id, name)
-    return HttpResponse(output)
+def index(request):
+    return render(request, "index.html")
+
--- helloapp/urls.py
+++ helloapp/urls.py
@@ -1,29 +1,9 @@
-"""helloapp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin # импортируется класс AdminSite, который предоставляет возможности работы с интерфейсом администратора.
-from django.urls import path# Эта функция задает сопоставление определенного маршрута с функцией обработки.
-from django.urls import re_path
+from django.contrib import admin
+from django.urls import path
 from firstapp import views
+
 urlpatterns = [
-
-    path('products/',views.products),
-    path('products/<int:productid>/',views.products),
-
-    path('users/',views.users),
-    path('users/<int:id>/<str:name>/',views.users),
+    path('', views.index),
 
 
 ]
