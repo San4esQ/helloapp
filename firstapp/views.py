32e578c8b82ae7bbf186e386e96d481ddb014294
Alexsandr Voronkov <voronkov_sasha2908@mail.ru>
2022-06-11 19:54:01 +0500

    create get response

--- firstapp/views.py
+++ firstapp/views.py
@@ -12,5 +12,18 @@ def index(request):
             return HttpResponse("<h2>Hello {0}, you are {1} y.o.</h2>".format(name, age))
     return render(request, "index.html", {"form": userForm})
 
-def test(request):
-    return render(request,"test.html")
+
+def users(request):
+    data = {'login': request.GET.get('login'), "password": request.GET.get('password')}
+
+    response = HttpResponse()
+    response.write(data['login']),
+    response.write(data['password'])
+
+    return response
+
+
+def roles(request):
+    return render(request, "roles.html")
+
+
